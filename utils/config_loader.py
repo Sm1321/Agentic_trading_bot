765d456966f6e38bafe4f85b30c1efdee9b6f309
SM1321 <siddulamohan1321@gmail.com>
2025-05-24 23:40:04 +0530

    updated

--- utils/config_loader.py
+++ utils/config_loader.py
@@ -1,6 +1,6 @@
 import yaml
 
-def load_config(config_path: str = r"C:\\agentic-trading-bot\\config\\config.yaml") -> dict:
+def load_config(config_path: str = r"C:\MY_Folder\MY_Courses\Agentic_trading_bot\config\config.yaml") -> dict:
     with open(config_path, "r") as file:
         config = yaml.safe_load(file)
     return config
